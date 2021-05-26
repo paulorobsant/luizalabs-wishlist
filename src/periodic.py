838a0c33d712fae06e74bdea714bcaaddded7087
Luiz Guilherme Picorelli <picorelli.luiz@gmail.com>
2021-05-25 23:54:57 -0300

    Add some improvements

--- src/periodic.py
+++ src/periodic.py
@@ -11,12 +11,12 @@ app = Celery("periodic", broker=settings.RABBIT_URL, include=["match.tasks"])
 app.conf.beat_schedule = {
     f"{settings.TASK_RECOMMENDATION_FIND_CONNECTION}-every-1-minute": {
         "task": settings.TASK_RECOMMENDATION_FIND_CONNECTION,
-        "schedule": 10.0,
+        "schedule": settings.SCHEDULE_RECOMMENDATION_FIND_CONNECTION,
         "options": {"queue": settings.QUEUE_RECOMMENDATIONS}
     },
     f"{settings.TASK_ALERT_CONNECTION}-every-1-minute": {
         "task": settings.TASK_ALERT_CONNECTION,
-        "schedule": 10.0,
+        "schedule": settings.SCHEDULE_TASK_ALERT_CONNECTION,
         "options": {"queue": settings.QUEUE_EMAILS}
     },
 }
