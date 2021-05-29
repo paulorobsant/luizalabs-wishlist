b42dfe215e0a63f18056a6b7c907b24cb6d092e8
Luiz Guilherme Picorelli <picorelli.luiz@gmail.com>
2021-05-29 17:58:48 -0300

    Add new SQL files

---
+++ src/seeds/input_companies.sql
@@ -0,0 +1,22 @@
+INSERT INTO public.company (id,created_at,updated_at,is_deleted,"name",email_suffixes) VALUES
+	 ('94599c94-7c2f-4f2c-ae97-7d7f9f609840','2021-05-18 03:19:01.344007','2021-05-18 03:19:01.344007',false,'QI ao Cubo','{qi3.com.br}'),
+	 ('bfcf274c-1cc8-4abc-9057-d1ef93fc90b1','2021-05-18 03:19:01.354781','2021-05-18 03:19:01.354781',false,'CBL','{lotescbl.com.br}'),
+	 ('e8d74dc0-fe34-4b5a-9e11-d914b9d54806','2021-05-18 03:19:01.362634','2021-05-18 03:19:01.362634',false,'Cinco Estrelas Construtora e Incorporadora LTDA','{cincoestrelasconstrutora.com.br}'),
+	 ('41cfeaae-eda7-4eaa-bdbb-9335f7bed8b8','2021-05-18 03:19:01.370083','2021-05-18 03:19:01.370083',false,'Control Ambiental','{controlambiental.com.br}'),
+	 ('eab50256-dde3-41e7-af3e-cafc33aae69e','2021-05-18 03:19:01.377308','2021-05-18 03:19:01.377308',false,'Econservation','{econservation.com.br}'),
+	 ('f45a5e8e-0aad-426a-b8a2-9cb86a5da37e','2021-05-18 03:19:01.384803','2021-05-18 03:19:01.384803',false,'Epix Engenharia','{epixengenharia.com.br}'),
+	 ('ff4ef993-42a5-4762-a93c-c0611882a5b5','2021-05-18 03:19:01.391892','2021-05-18 03:19:01.391892',false,'Fortes Engenharia','{fortes.ind.br}'),
+	 ('a3c37789-a648-4f3d-bf05-fc4536fbb243','2021-05-18 03:19:01.399278','2021-05-18 03:19:01.399278',false,'Galwan','{galwan.com.br}'),
+	 ('08dcfac7-371f-46b9-b529-f741f187856f','2021-05-18 03:19:01.407231','2021-05-18 03:19:01.407231',false,'Inocoopes','{inocoopes.com.br}'),
+	 ('d0be2ca9-b844-4f34-b189-1f2b7e235cf5','2021-05-18 03:19:01.415029','2021-05-18 03:19:01.415029',false,'Intelliway Tecnologia','{intelliway.com.br}');
+INSERT INTO public.company (id,created_at,updated_at,is_deleted,"name",email_suffixes) VALUES
+	 ('e7770ccd-b65b-4821-8326-a0ddfbda6bda','2021-05-18 03:19:01.422525','2021-05-18 03:19:01.422525',false,'JPF Engenharia','{jpfeng.com.br}'),
+	 ('c71731ff-c6a3-4471-8d02-b842960ec1f5','2021-05-18 03:19:01.43037','2021-05-18 03:19:01.43037',false,'Metasys Sistemas','{metasys.ind.br}'),
+	 ('6ee0f477-0556-4b14-b19b-ca04e4a93937','2021-05-18 03:19:01.437694','2021-05-18 03:19:01.437694',false,'Metron Engenharia','{metronemgenharia.com.br}'),
+	 ('1aa769dc-496d-4d59-b76b-17c2d714785a','2021-05-18 03:19:01.444911','2021-05-18 03:19:01.444911',false,'NÓS','{escritorionos.com.br}'),
+	 ('8378c28f-3bdc-4111-83c3-91549d3d5bcc','2021-05-18 03:19:01.451658','2021-05-18 03:19:01.451658',false,'Serrabetume Engenharia LTDA','{serrabetume.com.br}'),
+	 ('f4b1db19-e536-4389-9bde-e10742686838','2021-05-18 03:19:01.458476','2021-05-18 03:19:01.458476',false,'Sollo Brasil','{sollobrasil.com.br}'),
+	 ('c1641348-4bf7-496a-aff9-c9081f86e115','2021-05-18 03:19:01.465747','2021-05-18 03:19:01.465747',false,'Soma Urbanismo','{somaurbanismo.com.br}'),
+	 ('4d75c8ae-be9a-4050-a530-19a0c373378a','2021-05-18 03:19:01.472626','2021-05-18 03:19:01.472626',false,'Time-now','{timenow.com.br}'),
+	 ('0f7f3967-d33b-436b-a9c5-4f572dea6a2e','2021-05-18 03:19:01.479352','2021-05-18 03:19:01.479352',false,'VTO Polos Empresariais LTDA','{polosvto.com.br}'),
+	 ('ddb19bde-9af3-4f68-b0ed-a8fdcf2b5188','2021-05-18 03:19:01.486194','2021-05-18 03:19:01.486194',false,'CS3 Revestimentos','{cs3granitos.com.br}');
---
+++ src/seeds/input_terms.sql
@@ -0,0 +1,68 @@
+INSERT INTO public.match_term (id,created_at,updated_at,is_deleted,"label",value,is_approved) VALUES
+	 ('4c24835a-804b-4ad9-ac45-1e3fcdb3dfa4','2021-05-18 03:19:00.720492','2021-05-18 03:19:00.720492',false,'gestão de pessoas',0,false),
+	 ('131cdaf5-61d7-4664-bd6a-c767bdc470a2','2021-05-18 03:19:00.735331','2021-05-18 03:19:00.735331',false,'gestão de processos',1,false),
+	 ('5ca854fa-3a37-428c-8ef3-92c444efa670','2021-05-18 03:19:00.745676','2021-05-18 03:19:00.745676',false,'inovação',2,false),
+	 ('634b4992-08ef-4ad7-afe9-32debcb22df9','2021-05-18 03:19:00.754905','2021-05-18 03:19:00.754905',false,'planejamento estratégico',3,false),
+	 ('03386b96-8483-4fd5-a010-576d597f2876','2021-05-18 03:19:00.763776','2021-05-18 03:19:00.763776',false,'métricas',4,false),
+	 ('3025958d-2ce1-4d6a-b3d4-606b0b78fcdb','2021-05-18 03:19:00.772571','2021-05-18 03:19:00.772571',false,'captação de investimento',5,false),
+	 ('ee448fcc-3008-41a9-b5fb-9bc733876aea','2021-05-18 03:19:00.781265','2021-05-18 03:19:00.781265',false,'finanças',6,false),
+	 ('99ef0389-47ee-42f2-b726-e004446c925f','2021-05-18 03:19:00.789772','2021-05-18 03:19:00.789772',false,'administrativo',7,false),
+	 ('ea30c2e5-2402-4c3b-b130-717a434fc430','2021-05-18 03:19:00.798451','2021-05-18 03:19:00.798451',false,'comercial',8,false),
+	 ('823927a1-a321-4e0f-93ce-25c2a5879f56','2021-05-18 03:19:00.807316','2021-05-18 03:19:00.807316',false,'experiência do cliente',9,false);
+INSERT INTO public.match_term (id,created_at,updated_at,is_deleted,"label",value,is_approved) VALUES
+	 ('f6e7a5fe-4b97-4391-a723-9ebec6d85d73','2021-05-18 03:19:00.815914','2021-05-18 03:19:00.815914',false,'apoio emocional',10,false),
+	 ('50798ecf-eaf7-4b2b-ab85-1dfb6b30ab28','2021-05-18 03:19:00.826319','2021-05-18 03:19:00.826319',false,'recursos humanos',11,false),
+	 ('05e2d241-3d73-4af2-b5d7-07db96485ff9','2021-05-18 03:19:00.834746','2021-05-18 03:19:00.834746',false,'computação em nuvem',12,false),
+	 ('165decdd-40fe-49c4-98cd-350f4b2fe2fb','2021-05-18 03:19:00.84441','2021-05-18 03:19:00.84441',false,'robot process automation',13,false),
+	 ('28f8e2a6-4f9d-489d-ab84-08f14596b91d','2021-05-18 03:19:00.853008','2021-05-18 03:19:00.853008',false,'inteligência artificial',14,false),
+	 ('b2ae348a-3a0d-4f8c-96b1-a863d5d40417','2021-05-18 03:19:00.861499','2021-05-18 03:19:00.861499',false,'machine learning',15,false),
+	 ('9983b713-cd85-47b9-9e2f-b8377b2cf4b1','2021-05-18 03:19:00.870023','2021-05-18 03:19:00.870023',false,'deep learning',16,false),
+	 ('a65f027e-0a80-4c82-ae09-75fde0b37798','2021-05-18 03:19:00.878816','2021-05-18 03:19:00.878816',false,'big data',17,false),
+	 ('c42cfb71-eec6-402d-870e-1f1013e91b38','2021-05-18 03:19:00.887599','2021-05-18 03:19:00.887599',false,'data analytics',18,false),
+	 ('9e7e92e5-84dc-4667-a2cd-df057efb75ce','2021-05-18 03:19:00.896279','2021-05-18 03:19:00.896279',false,'iot',19,false);
+INSERT INTO public.match_term (id,created_at,updated_at,is_deleted,"label",value,is_approved) VALUES
+	 ('65e62138-cb33-4e80-ad52-2edd634a442b','2021-05-18 03:19:00.904702','2021-05-18 03:19:00.904702',false,'aprendizagem móvel',20,false),
+	 ('e74f43e0-334f-458a-b6ab-64055710d3b7','2021-05-18 03:19:00.913427','2021-05-18 03:19:00.913427',false,'jogos digitais',21,false),
+	 ('086aefe5-f4b5-4786-a1db-826364c44165','2021-05-18 03:19:00.923637','2021-05-18 03:19:00.923637',false,'realidade estendida',22,false),
+	 ('ec3ac851-e061-48a1-a41a-54f592709ebe','2021-05-18 03:19:00.934346','2021-05-18 03:19:00.934346',false,'impressão 3d',23,false),
+	 ('d7a9cf77-58c1-46b6-a35e-529f514bfc80','2021-05-18 03:19:00.944369','2021-05-18 03:19:00.944369',false,'robótica',24,false),
+	 ('feb4f778-09fa-42d6-8b9b-a9eed07d7150','2021-05-18 03:19:00.953326','2021-05-18 03:19:00.953326',false,'cibersegurança & privacidade',25,false),
+	 ('4c2e28eb-bdf0-46c9-8c60-3e4a976a40ce','2021-05-18 03:19:00.963143','2021-05-18 03:19:00.963143',false,'blockchain',26,false),
+	 ('83653a3c-1000-444b-af67-4924acb9739a','2021-05-18 03:19:00.972641','2021-05-18 03:19:00.972641',false,'conectividade & 5g',27,false),
+	 ('2bba3c97-8cc7-4964-bc14-bafa59bade7b','2021-05-18 03:19:00.982259','2021-05-18 03:19:00.982259',false,'plataforma de aprendizagem adaptativa',28,false),
+	 ('573772dd-2f1e-4a9c-a8db-c2080ae1ce7e','2021-05-18 03:19:00.991368','2021-05-18 03:19:00.991368',false,'learning analytics',29,false);
+INSERT INTO public.match_term (id,created_at,updated_at,is_deleted,"label",value,is_approved) VALUES
+	 ('700768ef-49a7-40fd-a536-662e92f80965','2021-05-18 03:19:01.000692','2021-05-18 03:19:01.000692',false,'ciência de dados',30,false),
+	 ('b59898bb-0cc9-44ff-8500-1a3999a578a0','2021-05-18 03:19:01.010219','2021-05-18 03:19:01.010219',false,'impacto social',31,false),
+	 ('41852a4e-848a-43f0-aabd-bfebefdcf801','2021-05-18 03:19:01.019373','2021-05-18 03:19:01.019373',false,'leis de incentivo',32,false),
+	 ('89bc146f-e7e3-4615-9446-9f8c433f3ad2','2021-05-18 03:19:01.02848','2021-05-18 03:19:01.02848',false,'maturidade em inovação',33,false),
+	 ('e68c8d86-769b-459a-b24c-ec31055f882b','2021-05-18 03:19:01.037224','2021-05-18 03:19:01.037224',false,'processos de inovação',34,false),
+	 ('4e15ba90-146e-4cf8-9905-12388d495cf7','2021-05-18 03:19:01.045844','2021-05-18 03:19:01.045844',false,'metodologias ágeis',35,false),
+	 ('c1f229fc-48f3-47a1-a16c-ad41bd673ee7','2021-05-18 03:19:01.054706','2021-05-18 03:19:01.054706',false,'bim',36,false),
+	 ('ed5a5864-8ac1-4b2c-bd52-6e4bea5c9c04','2021-05-18 03:19:01.063631','2021-05-18 03:19:01.063631',false,'gestão financeira',37,false),
+	 ('605730d6-9bb4-445d-a01b-89c0df1ffb15','2021-05-18 03:19:01.074548','2021-05-18 03:19:01.074548',false,'cadeia da construção civil',38,false),
+	 ('2eea7e30-db38-4288-a267-7113b69cd1ff','2021-05-18 03:19:01.098087','2021-05-18 03:19:01.098087',false,'metas',39,false);
+INSERT INTO public.match_term (id,created_at,updated_at,is_deleted,"label",value,is_approved) VALUES
+	 ('66c3effb-9b3a-4455-8a72-657cb2992f57','2021-05-18 03:19:01.121302','2021-05-18 03:19:01.121302',false,'experiência do usuário',40,false),
+	 ('9371cab8-7cd4-4f0e-a8a1-2bc603499b08','2021-05-18 03:19:01.138446','2021-05-18 03:19:01.138446',false,'customer relationship management | crm',41,false),
+	 ('a5f483cd-0134-4bbc-ac32-6105b90f4402','2021-05-18 03:19:01.152484','2021-05-18 03:19:01.152484',false,'índices de acidentes',42,false),
+	 ('edfe8b71-640d-4ded-a519-da7884d27203','2021-05-18 03:19:01.166937','2021-05-18 03:19:01.166937',false,'cultura',43,false),
+	 ('8d7c8170-3f16-4943-9b66-f88b010c70c0','2021-05-18 03:19:01.179874','2021-05-18 03:19:01.179874',false,'gestão de suprimentos',44,false),
+	 ('cd63ee46-50ce-4256-abce-aad075753efd','2021-05-18 03:19:01.190066','2021-05-18 03:19:01.190066',false,'orçamento',45,false),
+	 ('5d3e0647-783a-4861-bcae-14b302d19524','2021-05-18 03:19:01.200005','2021-05-18 03:19:01.200005',false,'marketing',46,false),
+	 ('48a27fa5-068a-45b0-929d-bf6f88414924','2021-05-18 03:19:01.2103','2021-05-18 03:19:01.2103',false,'comunicação interna',47,false),
+	 ('aaa09d76-4b83-48f7-b42b-d1878b6ff57a','2021-05-18 03:19:01.220672','2021-05-18 03:19:01.220672',false,'gestão de obras',48,false),
+	 ('0cd8d0dc-ea20-4b49-8994-53dea0bc65f7','2021-05-18 03:19:01.230659','2021-05-18 03:19:01.230659',false,'ti',49,false);
+INSERT INTO public.match_term (id,created_at,updated_at,is_deleted,"label",value,is_approved) VALUES
+	 ('50383f64-b9a0-45e0-9731-7da5c196d5c2','2021-05-18 03:19:01.240525','2021-05-18 03:19:01.240525',false,'gestão de qualidade',50,false),
+	 ('7cf1f972-7d9e-40e7-8368-4900b087d8a9','2021-05-18 03:19:01.250368','2021-05-18 03:19:01.250368',false,'aprovação e licenciamento de projetos',51,false),
+	 ('311843c2-33b2-43cb-bbdd-ee0098961067','2021-05-18 03:19:01.260072','2021-05-18 03:19:01.260072',false,'sap',52,false),
+	 ('7ffccd40-6cad-468d-9d21-4aa29c25866b','2021-05-18 03:19:01.270039','2021-05-18 03:19:01.270039',false,'salesforce',53,false),
+	 ('d2234e82-7c3d-4290-aefc-0001a96c3b0a','2021-05-18 03:19:01.279127','2021-05-18 03:19:01.279127',false,'segurança do trabalho',54,false),
+	 ('4c632ba7-2362-4e62-b0ed-10500dd2d34a','2021-05-18 03:19:01.288084','2021-05-18 03:19:01.288084',false,'orçamentação',55,false),
+	 ('e1f13081-14a6-4250-ab57-85a805465715','2021-05-18 03:19:01.297101','2021-05-18 03:19:01.297101',false,'relacionamento com o cliente',56,false),
+	 ('bf251156-cbea-40e7-97cd-45f261d9185c','2021-05-18 03:19:01.306355','2021-05-18 03:19:01.306355',false,'relacionamento com cartórios',57,false),
+	 ('665b2cc8-ccec-4d42-a361-4aca78301f88','2021-05-18 03:19:01.315629','2021-05-18 03:19:01.315629',false,'erp',58,false),
+	 ('3b430b7a-ec07-4418-9baa-f1b843e44528','2021-05-18 03:19:01.325269','2021-05-18 03:19:01.325269',false,'lei geral de proteção de dados | lgpd',59,false);
+INSERT INTO public.match_term (id,created_at,updated_at,is_deleted,"label",value,is_approved) VALUES
+	 ('5d3cb621-8717-411b-8a78-ed603535aa04','2021-05-18 03:19:01.334844','2021-05-18 03:19:01.334844',false,'okr',60,false);
